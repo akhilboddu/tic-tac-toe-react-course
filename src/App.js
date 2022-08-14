05e0d522b18a74db0ab853ed7bbae471766386f7
Akhil Boddu <akhil1234mara@gmail.com>
2022-08-14 12:22:21 +0200

    setup, tic tac toe logic, board styles, themeswitching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useContext } from "react";
+import Router from "./Router";
+import { GlobalStyles } from "./styles/Global.styled";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { ThemeContext } from "./contexts/ThemeContext";
+
 function App() {
+  const { theme } = useContext(ThemeContext);
+  const mode = (theme === "light" ? lightTheme : darkTheme);
+
   return (
-    <div>
-   Tic Tac Toe
-    </div>
+    <ThemeProvider theme={mode}>
+      <GlobalStyles />
+      <Router />
+    </ThemeProvider>
   );
 }
 
