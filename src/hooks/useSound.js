2f34b17e2c71d6c5cc70d3191f6040430b722eac
Akhil Boddu <akhil1234mara@gmail.com>
2022-08-15 13:57:24 +0200

    deployment fixes

--- src/hooks/useSound.js
+++ src/hooks/useSound.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 
 const useSound = (url, options) => {
   const [sound, setSound] = useState(false);
+
   useEffect(() => {
     const audio = new Audio(url);
 
     audio.load();
     audio.volume = options.volume;
     setSound(audio);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return () => {
     if (sound) {
